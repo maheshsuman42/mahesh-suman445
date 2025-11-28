45998f3f5cdbf820ae1d64ae6563c501990dcbf8
Mahesh Suman <ms427036@gmail.com>
2025-11-28 16:53:33 +0530

    5

--- index.js
+++ index.js
@@ -1,49 +1,50 @@
-import express from "express"
-import dotenv from "dotenv"
-dotenv.config()
-import connectDb from "./config/db.js"
-import cookieParser from "cookie-parser"
-import authRouter from "./routes/auth.routes.js"
-import cors from "cors"
-import userRouter from "./routes/user.routes.js"
+import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+import connectDb from "./config/db.js";
+import cookieParser from "cookie-parser";
+import authRouter from "./routes/auth.routes.js";
+import cors from "cors";
+import userRouter from "./routes/user.routes.js";
 
-import itemRouter from "./routes/item.routes.js"
-import shopRouter from "./routes/shop.routes.js"
-import orderRouter from "./routes/order.routes.js"
-import http from "http"
-import { Server } from "socket.io"
-import { socketHandler } from "./socket.js"
+import itemRouter from "./routes/item.routes.js";
+import shopRouter from "./routes/shop.routes.js";
+import orderRouter from "./routes/order.routes.js";
+import http from "http";
+import { Server } from "socket.io";
+import { socketHandler } from "./socket.js";
 
-const app=express()
-const server=http.createServer(app)
+const app = express();
+const server = http.createServer(app);
 
-const io=new Server(server,{
-   cors:{
-    origin:"https://fooddm.netlify.app".split(','),
-    credentials:true
-}
-})
-
-app.set("io",io)
-
+const io = new Server(server, {
+  cors: {
+    origin: ["https://fooddm.netlify.app"],
+    methods: ["GET", "POST"],
+    credentials: true
+  },
+});
 
 
-const port=process.env.PORT || 5000
-app.use(cors({
-    origin:"https://fooddm.netlify.app",
-    credentials:true
-}))
-app.use(express.json())
-app.use(cookieParser())
-app.use("/api/auth",authRouter)
-app.use("/api/user",userRouter)
-app.use("/api/shop",shopRouter)
-app.use("/api/item",itemRouter)
-app.use("/api/order",orderRouter)
+app.set("io", io);
 
-socketHandler(io)
-server.listen(port,()=>{
-    connectDb()
-    console.log(`server started at ${port}`)
-})
+const port = process.env.PORT || 5000;
+app.use(
+  cors({
+    origin: "https://fooddm.netlify.app",
+    credentials: true,
+  })
+);
+app.use(express.json());
+app.use(cookieParser());
+app.use("/api/auth", authRouter);
+app.use("/api/user", userRouter);
+app.use("/api/shop", shopRouter);
+app.use("/api/item", itemRouter);
+app.use("/api/order", orderRouter);
 
+socketHandler(io);
+server.listen(port, () => {
+  connectDb();
+  console.log(`server started at ${port}`);
+});
